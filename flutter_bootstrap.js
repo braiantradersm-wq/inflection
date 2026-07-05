5facb893cc1ed40e90bba9f0e114babe46eb61bb
Braian <openyoumindai@gmail.com>
2026-07-05 22:16:01 +0200

    ECG v5: oeste absoluto + filamento fino cian + toggle retenido 1.4x + onda halo/particulas

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1069115331" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "217180990" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
